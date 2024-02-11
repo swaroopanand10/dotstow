133f9966354dd798a8c1caafb266bccd955a1d09
swaroopanand <swaroopanand695@gmail.com>
2024-02-11 06:28:02 +0530

    added a script for installing nvim from source

---
+++ common/scripts/utilities/nvim_from_source.sh
@@ -0,0 +1,5 @@
+([ -d $HOME/repos/neovim ] || (git clone https://github.com/neovim/neovim.git $HOME/repos/neovim))
+cd $HOME/repos/neovim &&
+([ -d $HOME/repos/neovim/build ] && rm -r build/)
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/repos/neovim" &&
+make install
