758e4fbd36914a69228161fc3afbcbda6bb14cae
swaroopanand <swaroopanand@email.com>
2024-02-29 02:53:18 +0530

    edited to include gitpull in nvim_from_source script

--- common/scripts/utilities/nvim_from_source.sh
+++ common/scripts/utilities/nvim_from_source.sh
@@ -1,5 +1,5 @@
 ([ -d $HOME/repos/neovim ] || (git clone https://github.com/neovim/neovim.git $HOME/repos/neovim))
-cd $HOME/repos/neovim &&
+cd $HOME/repos/neovim && git pull &&
 ([ -d $HOME/repos/neovim/build ] && rm -r build/)
 make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/repos/neovim" &&
 make install
