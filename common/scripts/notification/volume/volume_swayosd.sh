f5746e73041bd8420ab66a40981c645060f850a5
swaroopanand <swaroopanand@email.com>
2024-04-29 18:15:21 +0530

    added swayosd to better control volume and brighness overlay screen, also added hyprspace plugin, also commented out previous keybinds

---
+++ common/scripts/notification/volume/volume_swayosd.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+function get_volume {
+	pamixer --get-volume
+}
+
+function is_mute {
+	pamixer --get-mute
+}
+
+function send_notification {
+	DIR=$(dirname "$0")
+	volume=$(get_volume)
+	bool_mute=$(is_mute)
+	if [ $bool_mute = "true" ]; then
+		DIR=$(dirname "$0")
+		$DIR/notify-send.sh -i "/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg" --replace=555 -u normal "Mute" -t 2000
+	else
+		if [ "$volume" = 0 ]; then
+			icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg"
+			$DIR/notify-send.sh "$volume" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"─" --replace=555
+		else
+			if [ "$volume" -lt "10" ]; then
+				icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
+			else
+				if [ "$volume" -lt "30" ]; then
+					icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
+				else
+					if [ "$volume" -lt "70" ]; then
+						icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-medium.svg"
+					else
+						icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-high.svg"
+					fi
+				fi
+			fi
+		fi
+	fi
+	## only send volume notfication if unmuted other wise send mute notification
+	if [ $bool_mute = "false" ]; then
+		bar=$(seq -s " " $(($volume)) | sed 's/[0-9]//g')
+		# Send the notification
+		$DIR/notify-send.sh "$volume" "$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"$bar" --replace=555
+	fi
+
+}
+
+case $1 in
+up)
+	# Set the volume on (if it was muted)
+	# pamixer -u
+	# Up the volume (+ 5%)
+	volume=$(get_volume)
+	bool_mute=$(is_mute)
+
+	if [ "$volume" -lt "65" ]; then
+		if [ $bool_mute = "false" ]; then
+			swayosd-client --output-volume +5
+		fi
+	fi
+	;;
+down)
+	bool_mute=$(is_mute)
+	if [ $bool_mute = "false" ]; then
+		swayosd-client --output-volume -5
+	fi
+	;;
+mute)
+	# Toggle mute
+	pamixer -t
+	pamixer --set-volume 0
+	bool_mute=$(is_mute)
+	if [ $bool_mute = "true" ]; then
+		swayosd-client --output-volume mute-toggle
+	fi
+	;;
+minimal) ## to set the volume at 30 instantly
+	bool_mute=$(is_mute)
+	if [ $bool_mute = "false" ]; then
+		pamixer --set-volume 30
+	fi
+	send_notification
+	;;
+esac
