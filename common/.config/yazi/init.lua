dcb2cf2e7093c4e392321d7fd6d85d354c34ef14
swaroopanand <swaroopanand@email.com>
2024-04-30 21:49:04 +0530

    yazi: changed exec to run in keymap.toml and yazi.toml as told by yazi headsup for upcoming update, also added some comment in init.lua

--- common/.config/yazi/init.lua
+++ common/.config/yazi/init.lua
@@ -1,9 +1,11 @@
+-- this code will disable the status line
 function Status:render() return {} end
 
 local old_manager_render = Manager.render
 function Manager:render(area)
 	return old_manager_render(self, ui.Rect { x = area.x, y = area.y, w = area.w, h = area.h + 1 })
 end
+-- end
 
 Header = {
 	area = ui.Rect.default,
