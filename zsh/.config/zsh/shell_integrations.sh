4fb2983661eb7fbb7ee1a33b57adadaff304ae5e
swaroopanand <swaroopanand@email.com>
2024-05-17 07:35:36 +0530

    zsh: small update with comment explaination

--- zsh/.config/zsh/shell_integrations.sh
+++ zsh/.config/zsh/shell_integrations.sh
@@ -1,5 +1,7 @@
 ## Shell integratoins
 
 eval "$(starship init zsh)"
-# eval "$(fzf --zsh)" # don't know what it does
 eval "$(zoxide init --cmd cd zsh)" # zoxide won't work without it
+
+# Now with new fzf update, the bindings present in ./fzfkeybinding.zsh is already loaded in binary, so to load them from binary this command is used, but I don't need this as I already have them in ./fzfkeybinding.zsh
+# eval "$(fzf --zsh)"
