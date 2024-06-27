465657b2abde89e3eee1daa0ba579bf77a79e1a7
swaroopanand <swaroopanand@email.com>
2024-06-27 21:34:16 +0530

    time_date: removed icons

--- common/scripts/notification/time_date.sh
+++ common/scripts/notification/time_date.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-icon="/usr/share/icons/Dracula/24/actions/clock-large.svg"
 title="$(date +%H:%M:%S)"
 text="$(date +%d), $(date +%B) $(date +%Y), $(date +%A)"
 
-notify-send -i $icon "$title" "$text"
+notify-send "$title" "$text"
