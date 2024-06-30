3cae185183f7198ab5855f61952ef7e3436baf5a
swaroopanand <swaroopanand@email.com>
2024-06-30 22:03:39 +0530

    changed zsh and tmux plugins dir

--- zsh/.config/zsh/plugins_git.sh
+++ zsh/.config/zsh/plugins_git.sh
@@ -1,12 +1,14 @@
 ### This file will manually source plugins installed in git repos###
 
+ZSH_PLUGIN_DIR="$HOME"/.local/plugins/zsh_plugins
+
 ### zsh fzf-tab
-source "$HOME/.local/share/zsh_plugin_dir/fzf-tab/fzf-tab.plugin.zsh"
+source "$ZSH_PLUGIN_DIR"/fzf-tab/fzf-tab.plugin.zsh
 
 ### zsh-auto-suggestions
-source "$HOME/.local/share/zsh_plugin_dir/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$ZSH_PLUGIN_DIR"/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 ### zsh syntax-highlighting
-source "$HOME/.local/share/zsh_plugin_dir/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$ZSH_PLUGIN_DIR"/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 ### zsh completions
